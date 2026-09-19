ffe5da8bc6240d401c46a14043803746f44aed32
fsanter <fsanter@student.tugraz.at>
2026-09-19 21:29:55 +0200

    Working testrunner

--- code/TestRunner.py
+++ code/TestRunner.py
@@ -1,5 +1,9 @@
+#Wichtig wenn d zu groß ist dann im c++ file ein buffer overflow
+
 import os
+import shutil
 import subprocess
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -10,20 +14,14 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 OUTPUT_DIR = os.path.join(SCRIPT_DIR, "output")
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "output.txt")
 
-LASPI_DATA_PATH = r"C:\Users\Abuscom\Desktop\Real-time-Anomaly-Detection-in-Time-Series-Data\LASPI-Detection_and_diagnostics_of_bearing_gear_and_combined_faults_of_gearbox\Healthy_motor\35hz_0%_2090rpm\acc_00001.csv"
-DATA_PATH = os.path.join(SCRIPT_DIR, "Data", "acc_00001.csv")
+LASPI_DATA_PATH = r"C:\Users\39320\workspace\Real-time-Anomaly-Detection-in-Time-Series-Data\LASPI-Detection_and_diagnostics_of_bearing_gear_and_combined_faults_of_gearbox\Healthy_motor\25hz_0%_1490rpm\acc_00001.csv"
+DATA_PATH = os.path.join(SCRIPT_DIR, "Data", "input.csv")
 
-FAULT_TYPE_NAME = os.path.basename(os.path.dirname(os.path.dirname(LASPI_DATA_PATH)))
-CONDITION_NAME = os.path.basename(os.path.dirname(LASPI_DATA_PATH))
 RESULTS_DIR = os.path.join(SCRIPT_DIR, "..", "benchmarkCode", "results")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 os.makedirs(RESULTS_DIR, exist_ok=True)
 os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
-
-df = pd.read_csv(LASPI_DATA_PATH, header=None)
-values = df[0]
-values.to_csv(DATA_PATH, header=False, index=False)
 
 
 def plot_output(png_path, title):
@@ -36,22 +34,31 @@ def plot_output(png_path, title):
     fig, ax = plt.subplots(figsize=(7, 7))
     ax.scatter(projected[:, 0], projected[:, 1], s=8, c=colormaps["turbo"](scores_norm), alpha=0.6)
     ax.axis("off")
-    ax.set_title(title, fontsize=30)
+    ax.set_title(title, fontsize=18)
 
     plt.tight_layout()
     plt.savefig(png_path)
     plt.show()
     plt.close(fig)
 
-DATASETS = {
-    "Dataset01":"acc_00001",
-}
 
-d = 13
+tau = 1
 
-for tau in [25,35]:
-    for windowSize in [8000]:
-        for label, fileName in DATASETS.items():
-            subprocess.run(["./anomaly_detection.exe", str(d), str(tau), str(windowSize), str(fileName)])
-            png_path = os.path.join(RESULTS_DIR, f"dynamic_{FAULT_TYPE_NAME}_{CONDITION_NAME}_tau_{tau}_w_{windowSize}_d{d}.png")
-            plot_output(png_path, f"w = {windowSize} d = {d} tau = {tau}")
+root_path = Path(LASPI_DATA_PATH).parents[2]
+
+for csv_file in root_path.glob("**/*.csv"):
+    if csv_file.name.startswith("._"):
+        continue
+
+    fault_type_name = csv_file.parent.parent.name
+    condition_name = csv_file.parent.name
+    fileName = csv_file.stem
+
+    shutil.copyfile(csv_file, DATA_PATH)
+    for d in [12,25,35]:
+        for windowSize in [8000]:
+            subprocess.run(["./anomaly_detection.exe", str(d), str(tau), str(windowSize), str("input")])
+            png_path = os.path.join(RESULTS_DIR,
+                                    f"dynamic_{fault_type_name}_{condition_name}_{fileName}_tau_{tau}_w_{windowSize}_d{d}.png")
+            plot_title = f"{fault_type_name}\n{condition_name}\nw = {windowSize} | d = {d} | tau = {tau}"
+            plot_output(png_path, plot_title)
